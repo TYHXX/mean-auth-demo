43bd8fd8fc6893826d2c35a38f96c64e18b6a428
Tian <Yao.Tian@weidmueller.com>
2018-11-15 17:03:12 +0800

    change database link to mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    // database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://adminuser:adminuser1234@ds155653.mlab.com:55653/mean-auth-demo',
     secret: 'yoursecret'
 }
